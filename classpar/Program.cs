8b0720937160fe12dc876bb7192eb1efc610d94e
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:40:23 -0500

    Agregar archivos de proyecto.

---
+++ classpar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class ParImpar
+    {
+        static void Main()
+        {
+            int numero;
+
+            Console.Write("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+            }
+        }
+    }
+}
+}
